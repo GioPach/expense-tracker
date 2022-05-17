65331c83791dfcdab441407dbd8d7a281130695a
Giovani <gigio.sig@gmail.com>
2022-05-17 13:58:40 -0300

    First Commit

---
+++ src/data/items.ts
@@ -0,0 +1,28 @@
+import { Item } from "../types/Item";
+
+export const items: Item[] = [
+  {
+    date: new Date(2022, 4, 16),
+    category: "food",
+    title: "BurkerKing",
+    value: 32.12,
+  },
+  {
+    date: new Date(2022, 4, 16),
+    category: "food",
+    title: "McDonals",
+    value: 28,
+  },
+  {
+    date: new Date(2022, 4, 17),
+    category: "rent",
+    title: "Alugel Apt",
+    value: 2300,
+  },
+  {
+    date: new Date(2022, 5, 17),
+    category: "salary",
+    title: "Salário ACME",
+    value: 4500,
+  },
+];
